15bba91f2ff79db98e3d637109d6543f541d11e8
MaxoPolo <snake@podderzka-irk.ru>
2020-06-04 10:50:35 +0800

    New write test12

--- task2015/Solution.java
+++ task2015/Solution.java
@@ -4,6 +4,22 @@ import java.io.*;
 
 /* 
 Переопределение сериализации
+Нашел в книге, что помогло с решением данной задачи.
+Однако, существует другое странное и хитрое решение.
+Используя встроенную возможность механизма сериализации, разработчики могут
+реализовать нормальный процесс поместив в свои файлы классов два метода:
+
+private void writeObject(ObjectOutputStream out) throws IOException;
+private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException;
+Обратите внимание, что оба метода (совершенно справедливо), объявлены как private,
+поскольку это гарантирует что методы не будут переопределены или перезагружены.
+Весь фокус в том, что виртуальная машина при вызове соответствующего метода автоматически проверяет,
+не были ли они объявлены в классе объекта. Виртуальная машина в любое время может вызвать private
+методы вашего класса, но другие объекты этого сделать не смогут.
+Таким образом обеспечивается целостность класса и нормальная работа протокол сериализации.
+Протокол сериализации всегда используется одинаково, путем вызова ObjectOutputStream.writeObject()
+или ObjectInputStream.readObject(). Таким образом, даже если в классе присутствуют эти специализированные
+private методы, сериализация объектов будет работать так же, как и для любых других вызываемых объектов.
 */
 public class Solution implements Serializable,Runnable {
     private transient Thread runner;
@@ -36,6 +52,8 @@ public class Solution implements Serializable,Runnable {
 
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
+        // теперь мы вновь получили "живой" объект, поэтому давайте перестроим и запустим его
+        //
         runner = new Thread(this);
         runner.start();
     }
