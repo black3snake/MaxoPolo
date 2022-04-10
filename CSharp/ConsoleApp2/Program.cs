fc18748cdbfc638824b5cdf6ad50bb0112eadef1
MaxoPolo <snake@podderzka-irk.ru>
2022-04-10 15:33:32 +0800

    New write test15

---
+++ CSharp/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        /*
+            Программа Первая:)
+         */
+        static void Main(string[] args) // Этот метод называется "Main" (Обязательно!)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.White;
+
+            // UNDONE: Доработать код
+            Console.WriteLine("Hello world");
+
+            // TODO: Добавить новую функциональность
+
+            Console.ResetColor();
+            Console.Write("Goodbye");
+
+            // HACK: Этот код, временное решение
+            Console.ReadKey();
+        }
+    }
+}
