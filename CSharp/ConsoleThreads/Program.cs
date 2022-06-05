bbfb2a0b513f6afe0fe02f74bc533fca106d8110
Black3snake <puhov.mv@rgufk.ru>
2022-06-05 12:57:45 +0800

    New write test15

---
+++ CSharp/ConsoleThreads/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleThreds
+{
+    internal class Program
+    {
+
+        // Метод выполняющийся во вторичном потоке
+        static void Function()
+        {
+            Console.WriteLine($"ID {Thread.CurrentThread.Name} запуск потока: {Thread.CurrentThread.ManagedThreadId}");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            for (int i = 0; i < 160; i++)
+            {
+                Thread.Sleep(20);
+                Console.Write(".");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($"{Thread.CurrentThread.Name} поток завершился");
+        }
+
+        static void Dowork(object obj)
+        {
+            Obj1 obj_tmp = (Obj1)obj;
+            
+            Console.WriteLine($"ID {Thread.CurrentThread.ManagedThreadId} запускаем поток");
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            for (int i = 0; i < obj_tmp.max; i++)
+            {
+                Thread.Sleep(20);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("+");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($"Имя принимаемого файла {obj_tmp.nameF}");
+            Console.WriteLine($" Поток {Thread.CurrentThread.ManagedThreadId} завершился");
+            obj_tmp.Print();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ID Первичного потока: {0}", Thread.CurrentThread.GetHashCode());
+
+            //Thread threadR = Thread.CurrentThread;
+            //Console.WriteLine($" имя род потока {threadR.Name}");
+
+            int kol_thred = 8;
+            var threads = new Thread[kol_thred];
+            //Создание нового потока
+            for (int t = 1; t < kol_thred; t++) {
+                Thread threadP = new Thread(new ThreadStart(Function));
+                threadP.Name = $"Thread{t}";
+
+                threads[t] = threadP;
+                threadP.Start();
+            }
+            Obj1 obj = new Obj1(260, @"C:\Users\snake\Desktop\test\1.txt");
+            Obj1 obj2 = new Obj1(180, @"C:\Users\snake\Desktop\test\2.txt");
+            //obj.max = 260;
+            //obj.nameF = @"C:\Users\snake\Desktop\test\1.txt";
+
+            Thread thread2 = new Thread(new ParameterizedThreadStart(Dowork));
+            thread2.Start(obj);
+
+            Thread thread3 = new Thread(Dowork);
+            thread3.Start(obj2);
+
+            // Ожидание Первычным потоком, завершение работы вторичного потока
+            //thread.Join(); //TODO Снять или установить комментарий
+            for (int t = 1; t < kol_thred; t++)
+            {
+                threads[t].Join();
+            }
+
+            thread2.Join();
+            thread3.Join();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            for (int i = 0; i < 160; i++)
+            {
+                Thread.Sleep(20);
+                Console.Write("-");
+            }
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("\nПервичный поток завершился.");
+
+            //Delay
+            Console.ReadKey();
+
+        }
+    
+    }
+    public class Obj1 
+    {
+        public int max;
+        public string nameF;
+        public Obj1(int m, string s) { max = m; nameF = s; }
+        
+        public void Print()
+        {
+            Console.WriteLine($"int max = {max}, string nameF = {nameF}");
+        }
+    }
+    
+}
---
+++ CSharp/IPwhois/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPwhois
+{
+    public partial class Form1 : Form
+    {
+        string addressG = "www.google.ru/maps/place/";
+        public Form1()
+        {
+            InitializeComponent();
+            // Быстрая обработка нажатия клавиши через Лямбду (внизу описано с методом, а здесь его создавать отдельно не надо) 
+            KeyPreview = true;
+            KeyDown += (s, e) => { if (e.KeyValue == (char)Keys.Enter) button1_Click(button1, null); };
+            textBox1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string line = "", lineLat = "", lineLon = "",address = "";
+            if (textBox1.Text != "")
+            {
+
+                if (!Regex.IsMatch(textBox1.Text.Substring(textBox1.Text.Length-1), @"\d"))
+                {
+                    MessageBox.Show("В конце должна стоять только цифра", Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                string[] TB = textBox1.Text.Split('.');
+                foreach(string t1 in TB)
+                {
+                    int t1int = Convert.ToInt32(t1);
+                    if(t1int >= 255)
+                    {
+                        MessageBox.Show("Неправельный диапазон", Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+
+                using (WebClient wc = new WebClient())
+                {
+                    //JSON
+                    line = wc.DownloadString($"https://ipwho.is/{textBox1.Text}?output=json");
+                }
+                //JSON
+                Match match = Regex.Match(line, "\"country\":\"(.*?)\",(.*?)region\":\"(.*?)\",(.*?)city\":\"(.*?)\",\"latitude\":(.*?),\"longitude\":(.*?),\"(.*?)isp\":\"(.*?)\",(.*?)utc\":\"(.*?)\",");
+                label10.Text = match.Groups[1].Value;
+                label11.Text = match.Groups[3].Value;
+                label12.Text = match.Groups[5].Value;
+                label13.Text = match.Groups[9].Value;
+                label14.Text = match.Groups[11].Value;
+                label15.Text = match.Groups[6].Value;
+                label16.Text = match.Groups[7].Value;
+
+                lineLat = match.Groups[6].Value;
+                lineLon = match.Groups[7].Value;
+
+                address = $"https://{addressG}{lineLat}+{lineLon}";
+
+
+                //webBrowser1.Navigate($"https://www.google.ru/maps/place/{lineLat}+{lineLon}"); //52%C2%B017'13.1%22N+104%C2%B018'18.1%22E
+
+                webBrowser1.Navigate(new Uri(address));
+                
+                if (Size.Width <= 1000)
+                {
+                    Size = new Size(1164, 708);
+                }
+            } else
+            {
+                MessageBox.Show("Введите ИП адрес", Text, MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(textBox1.Text, "[^0-9.]"))
+            {
+                MessageBox.Show("Только цифры", Text, MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+                textBox1.SelectionStart = textBox1.Text.Length;
+
+            }
+        }
+    }
+}
