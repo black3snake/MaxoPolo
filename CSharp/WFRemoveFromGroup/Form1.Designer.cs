216d6bce37a1bf49608dc53906207cf7a1b5ab6f
Black3snake <puhov.mv@rgufk.ru>
2022-09-15 10:10:24 +0800

    New write test15

---
+++ CSharp/WFRemoveFromGroup/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace WFRemoveFromGroup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtBLogin = new System.Windows.Forms.TextBox();
+            this.txtBPass = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.txtBConsole = new System.Windows.Forms.TextBox();
+            this.txtBGroup = new System.Windows.Forms.TextBox();
+            this.btnGroup = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pBox1 = new System.Windows.Forms.PictureBox();
+            this.pBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRemoveAll = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnGetAccouns = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox2)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtBLogin
+            // 
+            this.txtBLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBLogin.Location = new System.Drawing.Point(618, 12);
+            this.txtBLogin.Name = "txtBLogin";
+            this.txtBLogin.Size = new System.Drawing.Size(150, 22);
+            this.txtBLogin.TabIndex = 0;
+            // 
+            // txtBPass
+            // 
+            this.txtBPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBPass.Location = new System.Drawing.Point(618, 40);
+            this.txtBPass.Name = "txtBPass";
+            this.txtBPass.Size = new System.Drawing.Size(150, 22);
+            this.txtBPass.TabIndex = 0;
+            this.txtBPass.UseSystemPasswordChar = true;
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(17, 140);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(369, 255);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // txtBConsole
+            // 
+            this.txtBConsole.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBConsole.Location = new System.Drawing.Point(423, 140);
+            this.txtBConsole.Multiline = true;
+            this.txtBConsole.Name = "txtBConsole";
+            this.txtBConsole.ReadOnly = true;
+            this.txtBConsole.Size = new System.Drawing.Size(345, 255);
+            this.txtBConsole.TabIndex = 0;
+            // 
+            // txtBGroup
+            // 
+            this.txtBGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBGroup.Location = new System.Drawing.Point(50, 55);
+            this.txtBGroup.Name = "txtBGroup";
+            this.txtBGroup.Size = new System.Drawing.Size(305, 21);
+            this.txtBGroup.TabIndex = 0;
+            // 
+            // btnGroup
+            // 
+            this.btnGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGroup.Location = new System.Drawing.Point(361, 54);
+            this.btnGroup.Name = "btnGroup";
+            this.btnGroup.Size = new System.Drawing.Size(75, 23);
+            this.btnGroup.TabIndex = 2;
+            this.btnGroup.Text = "Get Info";
+            this.btnGroup.UseVisualStyleBackColor = true;
+            this.btnGroup.Click += new System.EventHandler(this.btnGroup_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(681, 401);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(87, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Location = new System.Drawing.Point(569, 15);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(43, 16);
+            this.lblLogin.TabIndex = 3;
+            this.lblLogin.Text = "Login:";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.Location = new System.Drawing.Point(542, 43);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(70, 16);
+            this.lblPass.TabIndex = 3;
+            this.lblPass.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(420, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Console output";
+            // 
+            // pBox1
+            // 
+            this.pBox1.BackgroundImage = global::WFRemoveFromGroup.Properties.Resources.slash_eye_icon_224538;
+            this.pBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pBox1.Location = new System.Drawing.Point(772, 39);
+            this.pBox1.Name = "pBox1";
+            this.pBox1.Size = new System.Drawing.Size(24, 23);
+            this.pBox1.TabIndex = 5;
+            this.pBox1.TabStop = false;
+            this.pBox1.Click += new System.EventHandler(this.pBox1_Click);
+            // 
+            // pBox2
+            // 
+            this.pBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pBox2.Location = new System.Drawing.Point(23, 54);
+            this.pBox2.Name = "pBox2";
+            this.pBox2.Size = new System.Drawing.Size(21, 23);
+            this.pBox2.TabIndex = 5;
+            this.pBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Удаление пользователей из группы в RIMS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Список пользователей в группе";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 398);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Count";
+            // 
+            // btnRemoveAll
+            // 
+            this.btnRemoveAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemoveAll.Location = new System.Drawing.Point(265, 401);
+            this.btnRemoveAll.Name = "btnRemoveAll";
+            this.btnRemoveAll.Size = new System.Drawing.Size(121, 23);
+            this.btnRemoveAll.TabIndex = 2;
+            this.btnRemoveAll.Text = "Remove Ac";
+            this.btnRemoveAll.UseVisualStyleBackColor = true;
+            this.btnRemoveAll.Click += new System.EventHandler(this.btnRemoveAll_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(230, 405);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(29, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "!";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnGetAccouns
+            // 
+            this.btnGetAccouns.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetAccouns.Location = new System.Drawing.Point(265, 111);
+            this.btnGetAccouns.Name = "btnGetAccouns";
+            this.btnGetAccouns.Size = new System.Drawing.Size(121, 23);
+            this.btnGetAccouns.TabIndex = 2;
+            this.btnGetAccouns.Text = "Get accounts";
+            this.btnGetAccouns.UseVisualStyleBackColor = true;
+            this.btnGetAccouns.Click += new System.EventHandler(this.btnGetAccouns_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(165, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(164, 22);
+            this.toolStripMenuItem1.Text = "Remove Selected";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 428);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(390, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Чтобы удалить учетные записи из группы, чекбокс должен быть заполнен.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(423, 428);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(221, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Время принятия изменений RIMS ~ 3 мин";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(749, 437);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 12);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "En+ Digital";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pBox2);
+            this.Controls.Add(this.pBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.btnGetAccouns);
+            this.Controls.Add(this.btnRemoveAll);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnGroup);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.txtBConsole);
+            this.Controls.Add(this.txtBGroup);
+            this.Controls.Add(this.txtBPass);
+            this.Controls.Add(this.txtBLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RemoveFromGroup";
+            ((System.ComponentModel.ISupportInitialize)(this.pBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox2)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBLogin;
+        private System.Windows.Forms.TextBox txtBPass;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox txtBConsole;
+        private System.Windows.Forms.TextBox txtBGroup;
+        private System.Windows.Forms.Button btnGroup;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pBox1;
+        private System.Windows.Forms.PictureBox pBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRemoveAll;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnGetAccouns;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
